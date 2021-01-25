986a168ff6380cb04cd24a9c5e5791e773c0af6d
furqanalishah <syedfurqan@wanclouds.net>
2021-01-25 22:43:18 +0500

    transfer listing now working fine

--- src/components/ServiceTransferList.js
+++ src/components/ServiceTransferList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import Card from '@material-ui/core/Card';
@@ -10,182 +10,195 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
-import { Tooltip, Zoom } from '@material-ui/core';
+import {Tooltip, Zoom} from '@material-ui/core';
 import PriceIcon from './PriceIcon';
 
 
-
 const useStyles = makeStyles((theme) => ({
-	root: {
-		margin: 'auto',
-	},
-	cardHeader: {
-		padding: theme.spacing(1, 2),
-	},
-	list: {
-		width: 200,
-		backgroundColor: theme.palette.background.paper,
-		overflow: 'auto',
-	},
-	item: {
-		padding: theme.spacing(0, 2),
-	},
-	button: {
-		margin: theme.spacing(0.5, 0),
-	},
+    root: {
+        margin: 'auto',
+    },
+    cardHeader: {
+        padding: theme.spacing(1, 2),
+    },
+    list: {
+        width: 200,
+        backgroundColor: theme.palette.background.paper,
+        overflow: 'auto',
+    },
+    item: {
+        padding: theme.spacing(0, 2),
+    },
+    button: {
+        margin: theme.spacing(0.5, 0),
+    },
 }));
 
 function not(a, b) {
-	return a.filter((value) => b.indexOf(value) === -1);
+    return a.filter((value) => b.indexOf(value) === -1);
 }
 
 function intersection(a, b) {
-	return a.filter((value) => b.indexOf(value) !== -1);
+    return a.filter((value) => b.indexOf(value) !== -1);
 }
 
 function union(a, b) {
-	return [...a, ...not(b, a)];
+    return [...a, ...not(b, a)];
 }
 
-const ServiceTransferList = ({ allServices, programDefaultServices }) => {
-	
+const ServiceTransferList = ({allServices, programDefaultServices}) => {
 
-	const classes = useStyles();
-	const [checked, setChecked] = React.useState([]);
-	const [left, setLeft] = React.useState(allServices);
-	const [right, setRight] = React.useState(programDefaultServices);
+    const hashService = ({id, name, price}) => `${id} ${name} ${price}`;
 
-	const leftChecked = intersection(checked, left);
-	const rightChecked = intersection(checked, right);
+    const hashSet = new Set(programDefaultServices.map(hashService));
 
-	const handleToggle = (value) => () => {
-		const currentIndex = checked.indexOf(value);
-		const newChecked = [...checked];
+    allServices = allServices.filter(p => !hashSet.has(hashService(p)));
 
-		if (currentIndex === -1) {
-			newChecked.push(value);
-			console.log(value);
-		} else {
-			newChecked.splice(currentIndex, 1);
-		}
 
-		setChecked(newChecked);
-	};
-	const numberOfChecked = (items) => intersection(checked, items).length;
+    const classes = useStyles();
+    const [checked, setChecked] = React.useState([]);
+    const [left, setLeft] = React.useState(allServices);
+    const [right, setRight] = React.useState(programDefaultServices);
+    const [prices, setPrices] = React.useState([]);
+    const [totalPrice, setTotalPrice] = React.useState(0);
 
-	const handleToggleAll = (items) => () => {
-		if (numberOfChecked(items) === items.length) {
-			setChecked(not(checked, items));
-		} else {
-			setChecked(union(checked, items));
-		}
-	};
+    const leftChecked = intersection(checked, left);
+    const rightChecked = intersection(checked, right);
 
-	const handleCheckedRight = () => {
-		setRight(right.concat(leftChecked));
-		setLeft(not(left, leftChecked));
-		setChecked(not(checked, leftChecked));
-	};
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
 
-	const handleCheckedLeft = () => {
-		setLeft(left.concat(rightChecked));
-		setRight(not(right, rightChecked));
-		setChecked(not(checked, rightChecked));
-	};
+        if (currentIndex === -1) {
+            newChecked.push(value);
+            console.log(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
 
-	const customList = (title, services) => (
-		<Card>
-			<CardHeader
-				className={classes.cardHeader}
-				avatar={
-					<Checkbox
-						onClick={handleToggleAll(services)}
-						checked={
-							numberOfChecked(services) === services.length &&
-							services.length !== 0
-						}
-						indeterminate={
-							numberOfChecked(services) !== services.length &&
-							numberOfChecked(services) !== 0
-						}
-						disabled={services.length === 0}
-						inputProps={{ 'aria-label': 'all items selected' }}
-					/>
-				}
-				title={title}
-				subheader={`${numberOfChecked(services)}/${services.length} selected`}
-			/>
-			<Divider />
-			<List className={classes.list} dense component='div' role='list'>
-				{services.map((service) => {
-					const labelId = `transfer-list-all-item-${service}-label`;
+        setChecked(newChecked);
+    };
+    const numberOfChecked = (items) => intersection(checked, items).length;
 
-					return (
-						<Tooltip
-							arrow
-							TransitionComponent={Zoom}
-							title={<PriceIcon price={service.price} />}
-						>
-							<ListItem
-								key={service.id}
-								role='listitem'
-								button
-								onClick={handleToggle(service)}
-								className={classes.item}
-							>
-								<ListItemIcon>
-									<Checkbox
-										checked={checked.indexOf(service) !== -1}
-										tabIndex={-1}
-										disableRipple
-										inputProps={{ 'aria-labelledby': labelId }}
-									/>
-								</ListItemIcon>
-								<ListItemText id={labelId} primary={service.name} />
-							</ListItem>
-						</Tooltip>
-					);
-				})}
-				<ListItem />
-			</List>
-		</Card>
-	);
-	//console.log({ left, right });
-	return (
-		<Grid
-			container
-			spacing={2}
-			justify='center'
-			alignItems='center'
-			className={classes.root}
-		>
-			<Grid item>{customList('Available Services', left)}</Grid>
-			<Grid item>
-				<Grid container direction='column' alignItems='center'>
-					<Button
-						variant='outlined'
-						size='small'
-						className={classes.button}
-						onClick={handleCheckedRight}
-						disabled={leftChecked.length === 0}
-						aria-label='move selected right'
-					>
-						&gt;
-					</Button>
-					<Button
-						variant='outlined'
-						size='small'
-						className={classes.button}
-						onClick={handleCheckedLeft}
-						disabled={rightChecked.length === 0}
-						aria-label='move selected left'
-					>
-						&lt;
-					</Button>
-				</Grid>
-			</Grid>
-			<Grid item>{customList('Program Default Services', right)}</Grid>
-		</Grid>
-	);
+    const handleToggleAll = (items) => () => {
+        if (numberOfChecked(items) === items.length) {
+            setChecked(not(checked, items));
+        } else {
+            setChecked(union(checked, items));
+        }
+    };
+
+    const handleCheckedRight = () => {
+        setRight(right.concat(leftChecked));
+        setLeft(not(left, leftChecked));
+        setChecked(not(checked, leftChecked));
+    };
+
+    const handleCheckedLeft = () => {
+        setLeft(left.concat(rightChecked));
+        setRight(not(right, rightChecked));
+        setChecked(not(checked, rightChecked));
+    };
+
+    const customList = (title, services) => (
+        <Card>
+            <CardHeader
+                className={classes.cardHeader}
+                avatar={
+                    <Checkbox
+                        onClick={handleToggleAll(services)}
+                        checked={
+                            numberOfChecked(services) === services.length &&
+                            services.length !== 0
+                        }
+                        indeterminate={
+                            numberOfChecked(services) !== services.length &&
+                            numberOfChecked(services) !== 0
+                        }
+                        disabled={services.length === 0}
+                        inputProps={{'aria-label': 'all items selected'}}
+                    />
+                }
+                title={title}
+                subheader={`${numberOfChecked(services)}/${services.length} selected`}
+            />
+            <Divider/>
+            <List className={classes.list} dense component='div' role='list'>
+                {services.map((service) => {
+                    const labelId = `transfer-list-all-item-${service}-label`;
+
+                    return (
+                        <Tooltip
+                            arrow
+                            TransitionComponent={Zoom}
+                            title={<PriceIcon price={service.price}/>}
+                        >
+                            <ListItem
+                                key={service.id}
+                                role='listitem'
+                                button
+                                onClick={handleToggle(service)}
+                                className={classes.item}
+                            >
+                                <ListItemIcon>
+                                    <Checkbox
+                                        checked={checked.indexOf(service) !== -1}
+                                        tabIndex={-1}
+                                        disableRipple
+                                        inputProps={{'aria-labelledby': labelId}}
+                                    />
+                                </ListItemIcon>
+                                <ListItemText id={labelId} primary={service.name}/>
+                            </ListItem>
+                        </Tooltip>
+                    );
+                })}
+                <ListItem/>
+            </List>
+        </Card>
+    );
+    return (
+        <Grid
+            container
+            spacing={2}
+            justify='center'
+            alignItems='center'
+            className={classes.root}
+        >
+            <Grid item>{customList('Available Services', left)}</Grid>
+            <Grid item>
+                <Grid container direction='column' alignItems='center'>
+                    <Button
+                        variant='outlined'
+                        size='small'
+                        className={classes.button}
+                        onClick={handleCheckedRight}
+                        disabled={leftChecked.length === 0}
+                        aria-label='move selected right'
+                    >
+                        &gt;
+                    </Button>
+                    <Button
+                        variant='outlined'
+                        size='small'
+                        className={classes.button}
+                        onClick={handleCheckedLeft}
+                        disabled={rightChecked.length === 0}
+                        aria-label='move selected left'
+                    >
+                        &lt;
+                    </Button>
+                </Grid>
+            </Grid>
+            <Grid item>{customList('Program Default Services', right)}</Grid>
+            <div className='float-right'>
+                Total Price: &nbsp;
+                <Button variant={'outlined'} color={'secondary'}>
+                    {setPrices(right.map((service) => service.price))}
+                    {setTotalPrice(totalPrice + prices.map(price => parseInt(price)))}
+                </Button>
+            </div>
+        </Grid>
+    );
 };
 export default ServiceTransferList;
--- src/components/StripeApi.js
+++ src/components/StripeApi.js
@@ -1,101 +1,81 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import React from 'react';
+import {toast} from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
-import { Card, Col, Container } from 'react-bootstrap';
+import {Card, Col, Container} from 'react-bootstrap';
 import room_img from '../assets/room_img.png';
 import Button from '@material-ui/core/Button';
 import ServiceTransferList from './ServiceTransferList';
-import useServices from '../hooks/useServices';
-import usePrograms from '../hooks/usePrograms';
 import useRoomsList from '../hooks/useRoomsList';
 
 toast.configure();
 
 const Stripe = (props) => {
-	const programs = props.location.state.programs;
-	const services = props.location.state.services;
-	const selectedRoomNumber = props.match.params.room;
-	const selectedProgramType = `${props.match.params.nights} DAYS`;
+    const programs = props.location.state.programs;
+    const services = props.location.state.services;
+    const selectedRoomNumber = props.match.params.room;
+    const selectedProgramType = `${props.match.params.nights} DAYS`;
 
-	const [rooms] = useRoomsList();
-	const selectedProgram = programs.filter(
-		(program) => program.type === selectedProgramType
-	);
-	
-	const programDefaultServices1 = selectedProgram.map((selectedProgram) => {
-		return selectedProgram.services.map((service) => {
-			return service;
-		});
-	});
-	const [programDefaultServices] = programDefaultServices1;
+    const programDefaultServices = programs.find((program) => program.type === selectedProgramType).services
 
-	const [room] = React.useState({
-		number: '2',
-		price: 20,
-		beds: 2,
-		capacity: 2,
-	});
+    const [room] = React.useState({
+        number: '2',
+        price: 20,
+        beds: 2,
+        capacity: 2,
+    });
 
-	//this callback ftn gets access to the token and addresses(billing and shipping address)
-	// async function handleToken(token, addresses) {
-	//     const response = await axios.post(
-	//         'https://ry7v05l6on.sse.codesandbox.io/checkout',
-	//         {token, room}
-	//     );
-	//     const {status} = response.data;
-	//     console.log('Response:', response.data);
-	//     //if we just want to show the status of payment.
-	//     if (status === 'success') {
-	//         //type means to make notification green.
-	//         toast('Success! Check email for details', {type: 'success'});
-	//     } else {
-	//         toast('Something went wrong', {type: 'error'});
-	//     }
-	// }
-	console.log({ services, programDefaultServices });
-	return (
-		<Container>
-			<Col className='flex-row align-items-center' lg={8}>
-				<Card>
-					<Card.Img variant={'top'} src={room_img} />
-					<Card.Body>
-						<Card.Title>
-							<strong>
-								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; All
-								Services&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-								{`${selectedProgramType} Program Services`}{' '}
-							</strong>
-						</Card.Title>
-						<Card.Subtitle>
-							{!services && !programDefaultServices ? null : (
-								<ServiceTransferList
-									allServices={services}
-									programDefaultServices={programDefaultServices}
-								/>
-							)}
-
-							<div className='float-right'>
-								Total Price: &nbsp;
-								<Button variant={'outlined'} color={'secondary'}>
-									$ 28
-								</Button>
-							</div>
-						</Card.Subtitle>
-					</Card.Body>
-					<Card.Footer>
-						<Button
-							className='float-right'
-							variant={'contained'}
-							color={'primary'}
-						>
-							Pay Now
-						</Button>
-					</Card.Footer>
-				</Card>
-			</Col>
-		</Container>
-	);
+    //this callback ftn gets access to the token and addresses(billing and shipping address)
+    // async function handleToken(token, addresses) {
+    //     const response = await axios.post(
+    //         'https://ry7v05l6on.sse.codesandbox.io/checkout',
+    //         {token, room}
+    //     );
+    //     const {status} = response.data;
+    //     console.log('Response:', response.data);
+    //     //if we just want to show the status of payment.
+    //     if (status === 'success') {
+    //         //type means to make notification green.
+    //         toast('Success! Check email for details', {type: 'success'});
+    //     } else {
+    //         toast('Something went wrong', {type: 'error'});
+    //     }
+    // }
+    console.log({services, programDefaultServices});
+    return (
+        <Container>
+            <Col className='flex-row align-items-center' lg={8}>
+                <Card>
+                    <Card.Img variant={'top'} src={room_img}/>
+                    <Card.Body>
+                        <Card.Title>
+                            <strong>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; All
+                                Services&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                {`${selectedProgramType} Program Services`}{' '}
+                            </strong>
+                        </Card.Title>
+                        <Card.Subtitle>
+                            {!services && !programDefaultServices ? null : (
+                                <ServiceTransferList
+                                    allServices={services}
+                                    programDefaultServices={programDefaultServices}
+                                />
+                            )}
+                        </Card.Subtitle>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button
+                            className='float-right'
+                            variant={'contained'}
+                            color={'primary'}
+                        >
+                            Pay Now
+                        </Button>
+                    </Card.Footer>
+                </Card>
+            </Col>
+        </Container>
+    );
 };
 export default Stripe;
